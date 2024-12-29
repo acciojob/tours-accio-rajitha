14785cd04e959b2fb7c37e0ef093a411da7b2b43
accio-rajitha <rajithasabhavath2019@gmail.com>
2024-12-29 14:51:29 +0530

    code

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -14,37 +14,10 @@ const Tour = ({ tour, removeTour }) => {
       <p id={`tour-item-para-${id}`}>
         {showInfo ? info : `${info.substring(0, 200)}...`}
         <button
-          id={`see-more-${id}`} // Dynamic ID added here
+          id={`see-more-${id}`} // Dynamic ID remains as expected by the test
           onClick={() => setShowInfo(!showInfo)}
         >
-          {showInfo ? 'Show less' : 'Read more'}
-        </button>
-      </p>
-      <button id={`delete-btn-${id}`} onClick={() => removeTour(id)}>
-        Not Interested
-      </button>
-    </article>
-  );
-};
-
-export default Tour;
-/*
-import React, { useState } from 'react';
-
-const Tour = ({ tour, removeTour }) => {
-  const [showInfo, setShowInfo] = useState(false);
-
-  const { id, name, info, image, price } = tour;
-
-  return (
-    <article>
-      <img src={image} alt={name} />
-      <h3>{name}</h3>
-      <h4>${price}</h4>
-      <p id={`tour-item-para-${id}`}>
-        {showInfo ? info : `${info.substring(0, 200)}...`}
-        <button onClick={() => setShowInfo(!showInfo)}>
-          {showInfo ? 'Show less' : 'Read more'}
+          {showInfo ? 'Show less' : 'See more'} {/* Updated button text */}
         </button>
       </p>
       <button id={`delete-btn-${id}`} onClick={() => removeTour(id)}>
